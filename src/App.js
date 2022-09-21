b7f9b1673aba0ebe897344268788d0934248f3c8
Renanbfreitas <renanbf1992@outlook.com>
2022-09-21 19:35:33 -0300

    Adicionando item 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Title />
+        <Missions />
       </>
     );
   }
