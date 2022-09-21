851b641b4607c48ede610b57158c37138de79019
Renanbfreitas <renanbf1992@outlook.com>
2022-09-21 19:14:24 -0300

    Adicionando item 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetas = Planets.map((planeta, index) => (
+      <PlanetCard
+        key={ index }
+        planetName={ planeta.name }
+        planetImage={ planeta.image }
+      />
+    ));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planetas }
       </div>
     );
   }
